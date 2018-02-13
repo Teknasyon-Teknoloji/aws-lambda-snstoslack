8094d69392e27cf21c73b3b601db6451170f724a
Fatih Üstündağ <fatih.ustundag@gmail.com>
2018-02-13 18:11:08 +0300

    json msg type support added

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const https = require('https');
 const url = require('url');
 var CONFIG = require("./config.json");
 
-exports.handler = function(event, context) {
+exports.handler = function (event, context) {
     (event.Records || []).forEach(function (rec) {
         if (rec.Sns) {
-            var slackUrl = CONFIG[rec.Sns.TopicArn];
-            if (slackUrl) {
+            var topicConfig = CONFIG[rec.Sns.TopicArn];
+            if (topicConfig) {
+                var slackUrl = topicConfig['slackHookUrl'];
                 var slackReqOpts = url.parse(slackUrl);
+                var slackText = '';
+                if (topicConfig['messageType'] === 'json') {
+                    slackText = JSON.stringify(JSON.parse(rec.Sns.Message), null, '  ').replace(/<br\s*\/?>/mg, "\n");
+                } else {
+                    slackText = JSON.stringify(rec.Sns.Message, null, '  ').replace(/<br\s*\/?>/mg, "\n");
+                }
                 slackReqOpts.method = 'POST';
                 slackReqOpts.headers = {'Content-Type': 'application/json'};
                 var req = https.request(slackReqOpts, function (res) {
@@ -18,11 +25,11 @@ exports.handler = function(event, context) {
                     }
                 });
 
-                req.on('error', function(e) {
+                req.on('error', function (e) {
                     console.log('problem with request: ' + e.message);
                     context.fail(e.message);
                 });
-                req.write(JSON.stringify({text: JSON.stringify(rec.Sns.Message, null, '  ').replace(/<br\s*\/?>/mg,"\n")}));
+                req.write(JSON.stringify({text: slackText}));
                 req.end();
             } else {
                 context.fail('Slack url not found! TopicARN' + rec.Sns.TopicArn);
